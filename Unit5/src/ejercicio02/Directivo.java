c32e49afff9c07afc0b0d39ffd6f608573540631
mrnkwar-sys <mr.nkwar@iesnervion.es>
2026-03-27 16:11:27 +0100

    ٩(ˊᗜˋ*)و

---
+++ Unit5/src/ejercicio02/Directivo.java
@@ -0,0 +1,12 @@
+package ejercicio02;
+
+public class Directivo extends Empleado{
+
+	Directivo(String nombre){
+		super(nombre);
+	}
+	
+	public String toStringDirectivo() {
+		return super.toStringEmpleado() + " -> Directivo";
+	}
+}
